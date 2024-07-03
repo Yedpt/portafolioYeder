07ee860c487d1824d33aa514c113bdeef9fa5acd
Yeder <ypimentel.tapia@gmail.com>
2024-07-03 14:25:19 +0200

    portafolio Yeder

---
+++ assets/JS/index.js
@@ -0,0 +1,11 @@
+let menuVisible = false;
+//funcion que oculta y muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
