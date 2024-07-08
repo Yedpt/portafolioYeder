cffa77ad6c30c55fab759396758fbbd75ccdf6c7
Yeder <ypimentel.tapia@gmail.com>
2024-07-08 12:12:13 +0200

    cambio de punto y coma en JS

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,11 +1,11 @@
-let menuVisible = false;
+let menuVisible = false
 //funcion que oculta y muestra el menu
 function mostrarOcultarMenu(){
     if(menuVisible){
-        document.getElementById("nav").classList ="";   /*classList: propiedad del objeto Element que nos permite acceder y manipular los datos de nodos HTML en el DOM. Esta propiedad en particular nos devuelve un listado de los valores del atributo class de un elemento (DOMTokenList). En este sentido es una propiedad de solo lectura. */
-        menuVisible = false;
+        document.getElementById("nav").classList =""   /*classList: propiedad del objeto Element que nos permite acceder y manipular los datos de nodos HTML en el DOM. Esta propiedad en particular nos devuelve un listado de los valores del atributo class de un elemento (DOMTokenList). En este sentido es una propiedad de solo lectura. */
+        menuVisible = false
     }else{
-        document.getElementById("nav").classList ="responsive";
-        menuVisible = true;
+        document.getElementById("nav").classList ="responsive"
+        menuVisible = true
     }
 }
